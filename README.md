7add4d1f796a8fdeec41ca55800a8d0d93ca85ba
Jovan Popovic <jocapc@gmail.com>
2017-09-09 21:27:43 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,98 @@
-# jquery-view-engine
-JQuery plugin view engine that binds JS objects into plain HTML templates
+# JQVEN - JQuery View ENgine
+
+**JQVEN** is probably the simplest possible JavaScript View Engine. There are no frameworks, or custom templating syntax. Just write the standard/plan HTML code that you want to show in the browser, take your JavaScript object that contains data and bind them.
+
+**JQVEN** is a view engine that enables you to do this.
+
+## Why yet another view engine?
+
+A long time ago, long before Angular, React and other templating engines, I needed a quick way to take JSON object via AJAX call and show it in the client side. I created a small library that loads JSON objects into HTML.
+
+Then AngularJS appeared, and I really liked it. I thought that Angular will be the solution for client-side templating and single-page applications (and to be honest it is in the most cases). Then Angular 2 appeared that became too heavy framework for my use cases. Yeah, it is great, but in some cases, it is too complex for some simple task.
+
+I looked at ReactJS and I like it too, but in some cases JSX syntax and mixing JavaScript and HTML in the same place is not something that I would use to simply bind the object to HTML code.
+
+With respect to mustache, vue.js, and other alternative templating engines - I really need something simpler.
+
+The question is - why we need a custom templating syntax? I believe that HTML is in most of the cases sufficient to describe template so I wanted to use standard HTML as a view template. This is idea behind **JQVEN** plugin - let's use plain HTML to define view template and just bind the object into the view to show the data.
+
+The only prerequisite is JQuery library that is used to bind object into HTML template.
+
+## Usage
+
+To use **JQVEN** you would need:
+ - HTML code that will be used as template. You can use standard HTML, HTML5 elements, Twitter Bootstrap, or your own HTML code.
+ - JSON object that will be used as a model that populates the HTML template.
+
+Once you have these two, you just need to convert the HTML template into the view and set the model object that will be bind:
+
+```html
+ $("div#myHtmlTemplate").view(model);
+```
+
+And that's it - your model object would be placed into the template. No overcomplicating - simple solution for the simple task. In the following sections you can see more details how to use it.
+
+### HTML template
+
+Template is a **plain HTML code**. Only requirement is that elements that should be populated must match the fields in model object by **id**, **name**, or **class**. An example of HTML template is shown below: 
+
+```html
+<div id="content">
+    <h1 id="Name"></h1>
+
+    <label for="Address">Address:</label>
+    <input name="Address"></input> 
+    
+    <label for="Contact">Contact by:</label>
+    <select name="Contact">
+        <option value="Email"/>
+        <option value="Phone"/>
+        <option value="Skype"/>
+    </select>
+</div>
+```
+
+The **h1** tag in the template has id *Name* and it would be used to display a name. input and select tags will be used to show Address and Contact method.
+
+### Model object
+
+Once HTML template is defined JSON object that will be used to populate template must be defines. Example of object that can populate template shown above is shown in the following listing: 
+```javascript
+var model = {
+    "Name":"ACME Co",
+    "Address":"Spanish Street 3, Belgrade",
+    "Contact":"Phone"
+}
+```
+This can be local object build in JavaScript code, or JSON object fetched from a web server via AJAX call.
+
+Object has three properties (Name, Address, and Contact) that will be injected into the template.
+
+## Binding JSON object to the template
+
+Once HTML template and JSON object are defined, JSON object can be loaded into the HTML code. This can be achieved using the following line of JavaScript code:
+
+```javascript
+$('div#content').view(model);
+```
+
+As a result, 'model' object will be loaded into the HTML fragment. Resulting HTML is shown in the following listing:
+
+```html
+<div id="content">
+    <h1 id="Name">ACME Co.</h1>
+    
+    <label for="Address">Address:</label>
+    <input name="Address" value="Spanish Street 3, Belgrade"></input> 
+    
+    <label for="Contact">Contact by:</label>
+    <select name="Contact">
+        <option value="Email"/>
+        <option value="Phone" selected/>
+        <option value="Skype"/>
+    </select>
+</div>
+```
+
+If this is also your use case, **JQVEN** is a view engine for you.
+**JQVEN** plugin works with the large number of HTML elements from plan SPAN, P, DIV, to various INPUT (text, checkbox, radio, HTML5 types), SELECT, TEXTAREA, etc.
---
+++ src/jquery.view-engine.js
@@ -0,0 +1,249 @@
+﻿/*
+* File:        jquery.view-engine.js
+* Version:     1.0
+* Author:      Jovan Popovic 
+* 
+* Copyright 2017 Jovan Popovic, all rights reserved.
+*
+* This source file is free software, under either the GPL v2 license or a
+* BSD style license, as supplied with this software.
+* 
+* This source file is distributed in the hope that it will be useful, but 
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+* or FITNESS FOR A PARTICULAR PURPOSE. 
+*
+* This file contains implementation of the JQuery templating engine that load JSON
+* objects into the HTML code. It is based on Alexandre Caprais notemplate plugin: 
+* with several enchancements that are added to this plugin.
+*/
+
+(function ($) {
+    $.fn.view = function (obj, options) {
+
+        function loadSelect(nSelect, aoValues, name) {
+            ///<summary>
+            ///Load options into the select list
+            ///</summary>
+            ///<param name="nSelect" type="select">Select list</param>
+            ///<param name="aoValues" type="Array{value,text,selected}">Array of object containin the options</param>
+            ///<param name="name" type="String">Name of the select list</param>
+            for (i = 0; i < aoValues.length; i++) {
+                $("<option/>")
+                    .attr("value", aoValues[i].value || aoValues[i])
+                    .text(aoValues[i].text || aoValues[i])
+                    .attr("selected", aoValues[i].selected)  
+                    .appendTo($(nSelect));
+            }
+        }
+
+        function setElementValue(element, value, name) {
+            var type = element.type || element.tagName;
+            if (type == null && element.length == 1) {
+                type = element[0].type || element[0].tagName; //select returns undefined if called directly
+            }
+            if (type==null)
+                return;
+            type = type.toLowerCase();
+            switch (type) {
+
+                case 'text':
+                case 'hidden':
+                case 'date':
+                case 'week':
+                case 'month':
+                case 'time':
+                case 'email':
+                case 'url':
+                case 'tel':
+                case 'color':
+                case 'datetime-local':
+                case 'number':
+                case 'range':
+                    $(element).val(value);
+                break;
+
+                case 'radio':
+                    if (value.toString().toLowerCase() == element.value.toLowerCase()) {
+                        $(element).attr("checked", "checked");
+                    }
+                break;
+
+                case 'checkbox':
+                    if (value) {
+                        $(element).attr("checked", true).attr("value",true);
+                    }
+                break;
+
+                case 'select':
+                case 'select-one':
+                case 'datalist':
+                    if (typeof value == "string" || typeof value == "number" || typeof value == "boolean") {
+                        $(element).val(value);
+                    } else if (value.constructor == Array) {
+                        loadSelect(element, value, name);
+                    } else {
+                        console.error("Cannot bind " + value + " to " + type);
+                    }
+                break;
+
+                case 'select-multiple':
+                    var select = element[0];
+                    if (element[0].options == null || typeof (element[0].options) == "undefined") {
+                        select = element;
+                    }
+                    if (select.options.length > 1) {
+                        //If select list is not empty use values array to select options
+                        var values = value.constructor == Array ? value : [value];
+                        for (var i = 0; i < select.options.length; i++) {
+                            for (var j = 0; j < values.length; j++) {
+                                select.options[i].selected |= select.options[i].value == values[j];
+                            }
+                        }
+                    } else {
+                        //ELSE: Instead of selecting values use values array to populate select list
+                        loadSelect(element, value, name);
+                    }
+                break;
+                
+                case 'option':
+                    var $option = $(element);
+                    // value can be object {value,text,selected} or scalar
+                    $option.attr("value", value.value || value); 
+                    $option.text(value.text || value.value || value);
+                    if (value.selected)
+                        $option.attr("selected", true);
+                break;
+
+                case 'a':
+                    var href = $(element).attr("href");
+                    var iPosition = href.indexOf('#');
+                    if (iPosition > 1000000) {
+                        href = href.substr(0, iPosition) + '&' + name + '=' + value + href.substr(iPosition)
+                    } else {
+                        iPosition = href.indexOf('?');
+                        if (iPosition > 0) // if parameters in the URL exists add new pair using &
+                            href += '&' + name + '=' + value;
+                        else//otherwise attach the name=value pair to the URL
+                            href = href + '?' + name + '=' + value;
+                    }
+                    $(element).attr("href", href);
+                    break;
+                case 'img':
+                    var $img = $(element);
+                    if (typeof value == "string") {
+                        //Assumption is that value is in the HREF$ALT format
+                        var iPosition = value.indexOf('$');
+                        var src = "";
+                        var alt = "";
+                        if (iPosition > 0) {
+                            src = value.substring(0, iPosition);
+                            alt = value.substring(iPosition + 1);
+                        }
+                        else {
+                            src = value;
+                        }
+                        $img.attr("src", src);
+                        $img.attr("alt", alt);
+                    } else {
+                        $img.attr("src", obj.src);
+                        $img.attr("alt", obj.alt);
+                        $img.attr("title", obj.title);
+                    }
+                    break;
+
+                case 'textarea':
+                case 'submit':
+                case 'button':
+                default:
+                    try {
+                        $(element).html(value.toString());
+                    } catch (exc) {
+                        console.error(exc);
+                    }
+            }
+        }
+
+        function bind(data, domNode, name) {
+
+            if (data == null)
+                return;
+
+            if (data.constructor == Object) {
+                if (domNode.length >= 1 && domNode[0].tagName == "OPTION")
+                {
+                    // Shortcut: if tag is OPTION and data is object - set the value of OPTION
+                    setElementValue(domNode[0], data, name);
+                    return;
+                }
+                else {
+                    for (var prop in data) {
+                        if (prop == null || typeof prop == "undefined")
+                            continue;
+                        else {
+                            //Find an element with class, id, or name that matches the property name
+                            var sSelector = ".bind-" + prop + ", #" + prop + ', [name="' + prop + '"]';
+                            bind(data[prop], jQuery(sSelector, domNode), prop);
+                            }
+                    }
+                }
+            }
+            else if (data.constructor == Array) {
+                if ( domNode.tagName == "SELECT" || domNode.tagName == "DATALIST"
+                    || domNode.length == 1 && (domNode[0].tagName == "SELECT" || domNode[0].tagName == "DATALIST")
+                ) {
+                    setElementValue(domNode, data, name);
+                    return;
+                } else
+                 {  
+                    // Clone the element that will be used as template (e.g. <li> or <tr>)
+                    var template = $(domNode).clone(true);
+                    for (var i = data.length - 1; i > 0 ; i--){
+                        // Put the clone of template on the second place and puth other after.
+                        var target = template.clone(true).insertAfter( $(domNode));
+                        // Bind i-th object into the new placeholder.
+                        bind(data[i], target, name);
+                    }
+                    // Bind 0-th object into the element used as a prototype (the first one).
+                    bind(data[0], $(domNode), name);
+                }
+            } // End Array
+            else {
+                // Scalar value
+                if (domNode.length > 0) {
+                    // If someone needs to bind scalar into multiple elements:
+                    for (var i = 0; i < domNode.length; i++)
+                        setElementValue(domNode[i], data, name);
+                }
+                else {
+                    setElementValue(domNode, data, name);
+                }
+            }
+        } //function bind() ends
+
+        function init(placeholder) {
+            if (placeholder.data("jquery-view-template") != null && placeholder.data("jquery-view-template") != "") {
+                var template = placeholder.data("jquery-view-template");
+                placeholder.html(template);
+            } else {
+                var template = placeholder.html()
+                placeholder.data("jquery-view-template", template);
+            }
+        }
+
+        var defaults = {
+            onLoading: jQuery.noop,
+            onLoaded: jQuery.noop
+        };
+
+        properties = $.extend(defaults, options);
+
+        return this.each(function () {
+
+            init($(this));
+            properties.onLoading();
+            bind(obj, this);
+            properties.onLoaded();
+            
+        });
+    };
+})(jQuery);
