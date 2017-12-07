f1feead2502ea66e54f2f03455aa78e3096a1ab3
Jovan Popovic <jocapc@gmail.com>
2017-12-07 21:18:07 +0100

    Added ajax+list example

---
+++ examples/api/companies.js
@@ -0,0 +1,27 @@
+[
+    {
+        "ID": 1,
+        "Name": "Emkay Entertainments",
+        "Address": "Nobel House, Regent Centre"
+    },
+    {
+        "ID": 2,
+        "Name": "The Empire",
+        "Address": "Milton Keynes Leisure Plaza"
+    },
+    {
+        "ID": 3,
+        "Name": "Asadul Ltd",
+        "Address": "Hophouse"
+    },
+    {
+        "ID": 4,
+        "Name": "Star Records",
+        "Address": "St Danny Street"
+    },
+    {
+        "ID": 5,
+        "Name": "Ashley Mark Publishing Company",
+        "Address": "1-2 Vance Court"
+    }
+]
